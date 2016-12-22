fccff32467354a521fbe43e935b100fa38c10e51
erwansimplon <guilleterwan@aol.com>
2016-12-22 15:44:50 +0100

    mise a jours ajout des routes pour les articles et le forum

--- src/Blog/BlogBundle/BlogBlogBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Blog\BlogBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BlogBlogBundle extends Bundle
-{
-}
--- src/Blog/GeneralBundle/BlogGeneralBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Blog\GeneralBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BlogGeneralBundle extends Bundle
-{
-}
